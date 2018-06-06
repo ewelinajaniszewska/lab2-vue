c64e2d1f39091b0419dec2ae07ee7352b612913c
ewelinajaniszewska <ewelinajaniszewska>
2018-06-06 23:57:52 +0200

    login zadanie

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,41 @@
 <template>
   <div id="app">
     <h1>Witaj w systemie do zapisow na zajecia!!</h1>
+   
+  <div v-if="logged">    
+   <h2>Twój e-mail to:  {{ email }}</h2> 
+      <button @click="logout()">Wyloguj</button>
+      </div>
 
-    <h4> 
-      <div v-if="email.length < 10">Ale masz krótki adres!</div>
-      <div v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>
-      <div v-else>Twój adres e-mail jest stanowczo za długi.</div>
-    </h4>
-    <h2>
-      Zaloguj sie emailem  <input type="email" v-model="email"> 
-
-    <button @click="alertMyEmail()">Wchodze </button> 
-
-    </h2>
-     
-  </div>
-
-</template>
-
-
-
-<script>
-export default {
-data() {
-  return {
-    email: ''
+ <div v-else>
+  <h2> Zaloguj sie emailem  <input type="email" v-model="email"> </h2>
+      <button @click="login()">Wchodzę</button>
+   </div>
+</div>
+ </template>
+ 
+ <script>
+ export default {
+ name: "app",
+ data() {
+     return {
+    email: '',
+    logged: false
   };
-},
-methods: {
-  alertMyEmail() {
-    alert(this.email);
-  }
-}
-}
+ },
+ methods: {
+  login() {
+    this.logged = true;
+  },
+   logout() {
+     this.logged = false;
+     }
+   }
 
-</script>
+};
+ </script> 
 
+      
 
 
 <style lang="scss">
